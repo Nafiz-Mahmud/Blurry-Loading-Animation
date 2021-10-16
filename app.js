91519408ef2b9e635e1f1499e701f00a579b939f
Nafiz Mahmud <91520170+Nafiz-Mahmud@users.noreply.github.com>
2021-10-16 12:55:26 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+const loadText = document.querySelector(".loading-text");
+const bg = document.querySelector(".bg");
+const main = document.querySelector(".main");
+
+main.addEventListener("mouseover", function () {
+  main.classList.add("active");
+});
